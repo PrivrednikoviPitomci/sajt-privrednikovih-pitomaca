7a858660179288ef6eaad3e5adf1e7131148718d
GoranKrajoski <krajoski@gmail,com>
2018-06-02 19:31:33 +0200

    no message

--- server/environment/environment.js
+++ server/environment/environment.js
@@ -1,7 +1,8 @@
 const env = {
     dbName: 'pitomci-db',
     key: '01yNUza2Mogc6hTgJLwL1Zld6Tekj22uqSbMKXAc85frRdYT64PLeTqA5Bww3ki36rvZMH4X6M7gtraxmi12HA==',
-    port: 10255
+    port: 10255,
+    ssl: true
 };
 
 module.exports = env;
